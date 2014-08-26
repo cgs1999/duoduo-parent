fa376a05040167836854f6f8606f33531c3d86de
cgs1999 <cgs1999@126.com>
2014-08-26 23:54:46 +0800

    add language testcase

---
+++ duoduo-business/src/test/java/com/duoduo/system/manager/TestLanguageManager.java
@@ -0,0 +1,12 @@
+package com.duoduo.system.manager;
+
+
+/**
+ * TODO
+ * @author chengesheng@gmail.com
+ * @date 2014-8-26 下午11:32:59
+ * @version 1.0.0
+ */
+public class TestLanguageManager {
+
+}
